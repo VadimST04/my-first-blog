5be37e61d3f247594139e1da0e0a2ad27420ec33
Vadym Starchak <vadikstarchak04@gmail.com>
2023-03-12 21:49:08 +0100

    add draft pages and publish delete buttons

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,22 +5,35 @@ from .forms import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+    """
+    :param request: a request that was sent by the client to the server
+    :return: show a page with list of posts
+    """
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
+    """
+    :param request: a request that was sent by the client to the server
+    :param pk: identifier of a specific article in the model
+    :return: show a page of a specific post
+    """
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post' : post})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
 def post_new(request):
+    """
+    :param request: a request that was sent by the client to the server
+    :return: if the form is valid then show a page of a specific post, if not then show a page of editing
+    """
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
+            post = form.save(commit=False)  # do not save post to db (without author name)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -29,15 +42,38 @@ def post_new(request):
 
 
 def post_edit(request, pk):
+    """
+    :param request: a request that was sent by the client to the server
+    :param pk: identifier of a specific article in the model
+    :return: if the form is valid then show a page of a specific post, if not then show a page of editing
+    """
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
+    if request.method == 'POST':
+        # 'instance' in order not to create a new post, but to edit this one
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
