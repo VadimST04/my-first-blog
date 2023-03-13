3b8b74631f0d1508a9b253633c39dad47a988f23
Vadym Starchak <vadikstarchak04@gmail.com>
2023-03-13 18:49:39 +0100

    add login logout pages

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -6,3 +6,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+        from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class': 'form-floating'}))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-floating'}))
